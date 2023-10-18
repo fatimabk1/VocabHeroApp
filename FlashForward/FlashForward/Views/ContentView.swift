ecf80aa73f718e6be96d3d171a68d14b854794bb
fatimabk1 <fatima.kahbi@gmail.com>
2023-10-18 12:54:11 -0400

    Updated app name in launch screen, updated app to target iphone

--- FlashForward/FlashForward/Views/ContentView.swift
+++ FlashForward/FlashForward/Views/ContentView.swift
@@ -20,7 +20,7 @@ struct ContentView: View {
                 MainView()
             }
             else {
-                LoadingView<ATTwistEffect>(elements: ["Flash Forward"])
+                LoadingView<ATTwistEffect>(elements: ["FlashWise"])
             }
         }
         .onChange(of: manager.isLoading) { loadStatus in
@@ -43,7 +43,7 @@ struct ContentView: View {
 
 struct LoadingView<E: ATTextAnimateEffect>: View {
     // text animation
-    @State var text: String = "FlashForward"
+    @State var text: String = "FlashWise"
     @State var type: ATUnitType = .letters
     @State var userInfo: Any? = nil
     let elements: [String]
